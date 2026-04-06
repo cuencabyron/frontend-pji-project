fcebb33eb05aa4e56defca7ba486fde9796e797c
byron.dev <byronjorge.158@gmail.com>
2026-04-05 22:50:55 -0600

    fix: url prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = 
 {
   production: true,
-  apiBaseUrl: 'http://ec2-54-242-13-56.compute-1.amazonaws.com:80/api',
+  apiBaseUrl: 'http://ec2-54-242-13-56.compute-1.amazonaws.com:3000/api',
 };
